17f70ceba832fa7bf2f621dee71c3ffd53e8b8cf
abdullahqadri1991 <abdullahqadri1991@gmail.com>
2023-10-08 08:57:27 -0700

    assignment4

---
+++ app.js
@@ -0,0 +1,34 @@
+
+// 3 variables in one statement.
+// var name, age, city;
+
+
+// 5 illegal variables
+// var 1991abdullah;
+// var abdullah Hashim ;
+// var abdullahHashim^madni;
+// var if = 1991;
+// var my-variable;
+
+
+//5 legal variables
+// var abdullahHashim;
+// var abdullah
+// var abdullah_Madni;
+// var abdullah1991HashimMadni;
+// var abdullah$HashimMadni;
+
+// display text in browser
+
+var headingStarting = "<h1>Rules for naming JS variables</h1>";
+var blank1 = " Variable names can only contain only  letters, numbers, dollar signs, and underscores  For example $my_1stVariable"
+var blank2 = " <br><br>Variable must begin with letters, $, and underscore. For example $name, _name or name"
+var blank3 = " <br><br>Variable names are case sensitive."
+var blank4 = " <br><br>Variable names should not be JS  Keywords ."
+
+    
+document.write(headingStarting);
+document.write(blank1); 
+document.write(blank2);
+document.write(blank3)
+document.write(blank4)
